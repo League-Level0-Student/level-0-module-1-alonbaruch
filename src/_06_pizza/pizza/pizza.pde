9a31b411efd6bea3e89001c2b171ea75d17c2e12
League Student <league@iMac-37.attlocal.net>
2019-08-06 19:59:58 -0700

    :sleepy:

--- src/_06_pizza/pizza/pizza.pde
+++ src/_06_pizza/pizza/pizza.pde
@@ -1,7 +1,12 @@
 
 void setup() {
-    
-}
+    size(500, 500);
+fill(#F0C92E);
+ellipse(250,250 , 200, 200);
+fill(#CB2F17);
+ellipse(250,250,175,175);
+fill(#F0E91D);
+ellipse(250,250,150,150);}
 void draw() {
-    
+
 }
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,20 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+int score = 0;
+score++;
+// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String a = JOptionPane.showInputDialog(null,"what has a head and a tail but no body");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (a.equals("a coin")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+} else {JOptionPane.showMessageDialog(null,"Wrong it is a cion");
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null,score++);
 	}
-}
+	}}
 
